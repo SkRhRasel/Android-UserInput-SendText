43917a345f72b10a622a7160e1e47b0330488463
SkRhRasel <sk.rasel.cse@gmail.com>
2019-03-26 21:55:05 +0600

    Android-UserInput-SendText

---
+++ app/src/main/java/com/example/rasel/androiduserinput/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.rasel.androiduserinput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "This is text from Toast", Toast.LENGTH_SHORT).show();
+    }
+}
